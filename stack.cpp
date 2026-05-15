338ac610180e66eb3f41aa3650d2418451cab007
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-05-15 15:20:05 +0700

    Implementasi pembuatan newnode pada push()

--- stack.cpp
+++ stack.cpp
@@ -17,7 +17,12 @@ class stack{
         top = NULL;
       }
     int push(int value){
-        
+       node *newnode = new node();
+        newnode -> data = value;
+        newnode -> next = top;
+        top - newnode;
+        cout << "push value : "<< value << endl;
+        return value; 
       }
 
 };
